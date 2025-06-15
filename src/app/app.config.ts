6726063753523a4517f1167cefc0ccbc3ce769ef
CALLMEWILDYYY\user <ouiam.moukhtar@gmail.com>
2025-06-15 13:52:55 +0100

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // ✅ Import nécessaire
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom(HttpClientModule) // ✅ C’est ça qui manquait !
+  ]
+};
